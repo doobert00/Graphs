1cdf6a86b50551b624136535909108491f3b9bfc
doobert <dubinskyz00@gmail.com>
2023-12-11 18:00:23 -0500

    start of floyd warshall

--- src/main/PathSolver.java
+++ src/main/PathSolver.java
@@ -127,8 +127,31 @@ public class PathSolver<V> {
 
     }
     //TODO: Write Floyd-Warshall Algorithm
-    public void FloydWarshall(){
-
+    public void FloydWarshall(Graph<V> G){
+        HashMap<V,HashMap<V,Integer>> dist = new HashMap<>();
+        //Initialize all entries to inf
+        for(V v : G.getVerticies()){
+            for(V u : G.getVerticies()){
+                dist.get(v).put(u,Integer.MAX_VALUE);
+            }
+        }
+        for(V v : G.getVerticies()){
+            for(V u : G.getNeighbors(v)){
+                dist.get(v).put(u,G.getWeight(v,u));
+            }
+        }
+        for(V v : G.getVerticies()){
+            dist.get(v).put(v,0);
+        }
+        for(int i = 0; i < G.numVerticies(); i++){
+            for(int j = 0; j < G.numVerticies(); j++){
+                for(int k = 1; k < G.numVerticies(); k++){
+//                    if dist[j][k] > dist[j][i] + dist[i][k]
+//                    dist[j][k] ← dist[j][i] + dist[i][k]
+                    break;
+                }
+            }
+        }
     }
     //TODO: Write Johnson's Algorithm
     public void Johnson(){
