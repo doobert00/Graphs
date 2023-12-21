934c58bad1c2e4eadce05ac0c2740a5aef74d0e5
doobert <dubinskyz00@gmail.com>
2023-12-21 16:15:50 -0500

    broken on bad paths

--- src/main/commands/FloydWarshall.java
+++ src/main/commands/FloydWarshall.java
@@ -2,36 +2,54 @@ package main.commands;
 
 import main.Graph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FloydWarshall<V> implements Command<V>{
-    //TODO: Write Floyd-Warshall Algorithm
+    //https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
+    /**
+     * Reconstructs the path between start and end using a hashmap computed in
+     * the course of the Bellman-Ford algorithm
+     * @param start
+     * @param end
+     * @return
+     */
     public Iterable<V> execute(Graph<V> G, V start, V end){
+        HashMap<V,HashMap<V,V>> prev = FloydWarshall(G, start, end);
+        ArrayList<V> path = new ArrayList<V>();
+        path.addFirst(end);
+        while(!start.equals(end)){
+            start = prev.get(start).get(end);
+            path.addFirst(start);
+        }
+        System.out.println(path.toString());
+        return path;
+    }
+    private HashMap<V,HashMap<V,V>> FloydWarshall(Graph<V> G, V start, V end){
         HashMap<V, HashMap<V,Integer>> dist = new HashMap<>();
-        //Initialize all entries to inf
-        for(V v : G.getVerticies()){
-            dist.put(v,new HashMap<V,Integer>());
-            for(V u : G.getVerticies()){
-                dist.get(v).put(u,Integer.MAX_VALUE);
-            }
-        }
-        for(V v : G.getVerticies()){
-            for(V u : G.getNeighbors(v)){
-                dist.get(v).put(u,G.getWeight(v,u));
-            }
-        }
-        for(V v : G.getVerticies()){
+        HashMap<V, HashMap<V,V>> prev = new HashMap<>();
+        for(V v: G.getVerticies()){
+            dist.put(v, new HashMap<V,Integer>());
+            prev.put(v, new HashMap<V,V>());
             dist.get(v).put(v,0);
+            prev.get(v).put(v,v);
+            for(V u: G.getNeighbors(v)){
+                dist.get(v).put(u,G.getWeight(v,u));
+                prev.get(v).put(u,v);
+            }
         }
-        for(int i = 0; i < G.numVerticies(); i++){
-            for(int j = 0; j < G.numVerticies(); j++){
-                for(int k = 1; k < G.numVerticies(); k++){
-//                    if dist[j][k] > dist[j][i] + dist[i][k]
-//                    dist[j][k] ← dist[j][i] + dist[i][k]
-                    break;
+        for(V v: G.getVerticies()){
+            for(V u: G.getVerticies()){
+                for(V w: G.getVerticies()){
+                    int tmp = dist.get(u).get(v) + dist.get(v).get(w);
+                    if(dist.get(u).get(w) > tmp){
+                        dist.get(u).put(w,tmp);
+                        prev.get(u).put(w,prev.get(v).get(w));
+                    }
                 }
             }
         }
-        return null;
+
+        return prev;
     }
 }
